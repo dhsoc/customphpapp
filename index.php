7ad4af453dcd5b7663b4b1fd39e8af7c8629ff49
dhsoc <49658384+dhsoc@users.noreply.github.com>
2019-04-18 18:56:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
             <li><a href="#">Home</a></li>
             <li>Basic</li>
           </ol>
-          <h1>Hello, Welcome to an OpenShift Application!</h1>
+          <h1>This is for test, Welcome to an OpenShift Application!</h1>
           <p>The purpose of this application is to demonstrate several interesting features about OpenShift. We hope you enjoy it!</p>
         </div>
       </div>
